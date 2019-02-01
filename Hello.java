5e2265558fd284f0613cc3dfb6d4bea178807680
workingclass <31182062+wangshaoliang@users.noreply.github.com>
2019-02-01 15:39:55 +0800

    delete
    del it

--- Hello.java
+++
@@ -1,9 +0,0 @@
-class  
-{
-	public static void main(String[] args) 
-	{
-		System.out.println("Hello World!");
-		System.out.println("second commit!");
-		System.out.println("third commit!");
-	}
-}
